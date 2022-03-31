75bc9828a71b1a15dcb9c5f1bb1882da4986e7d4
Dmitry Orlov <dimaorol@gmail.com>
2022-03-31 19:05:06 +0300

    push test

--- XamarinApp/XamarinApp.Android/MainActivity.cs
+++ XamarinApp/XamarinApp.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.FirebasePushNotification;
 
 namespace XamarinApp.Droid
 {
@@ -17,6 +18,38 @@ namespace XamarinApp.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            SetUpPush();
+        }
+
+        private void SetUpPush()
+        {
+            //Set the default notification channel for your app when running Android Oreo
+            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+            {
+                //Change for your default notification channel id here
+                FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
+
+                //Change for your default notification channel name here
+                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+            }
+
+
+            //If debug you should reset the token each time.
+#if DEBUG
+            FirebasePushNotificationManager.Initialize(this, true);
+#else
+            FirebasePushNotificationManager.Initialize(this,false);
+#endif
+
+            //Handle notification when app is closed here
+            var a = CrossFirebasePushNotification.Current.Token;
+            CrossFirebasePushNotification.Current.OnNotificationReceived += OnNotificationReceived;
+        }
+
+        private void OnNotificationReceived(object source, FirebasePushNotificationDataEventArgs e)
+        {
+            Console.WriteLine(e.Data);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
